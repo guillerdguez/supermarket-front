06b8231e6bdff0f407193b75a9f016e00874b3ed
guillerdguez <guillermordguezrdguez@gmail.com>
2026-08-06 13:52:29 +0200

    feat(transfer): add transfer domain model

--- src/app/model/Domain/transfer.model.ts
+++ src/app/model/Domain/transfer.model.ts
@@ -4,6 +4,7 @@ import { TransferResponse } from "../../DTO/transfer.dto";
 @Injectable({ providedIn: "root" })
 export class TransferModel {
   readonly list = signal<TransferResponse[]>([]);
+  readonly mine = signal<TransferResponse[]>([]);
   readonly loading = signal(false);
   readonly error = signal<string | null>(null);
 }
